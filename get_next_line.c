47e63b8b51763b65afdefa33db8758aea7f89605
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-28 13:00:51 +0200

    Manages to break most things

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 13:02:13 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/27 17:59:45 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/28 12:52:36 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static int	check(int fd, char *buffer)
 {
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd > 512 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
-		buffer[0] = 0;
+		*buffer = 0;
 		return (1);
 	}
 	return (0);
@@ -29,12 +29,11 @@ static char	*clean(char *next_line)
 		if (*next_line)
 			return (next_line);
 		free(next_line);
-		next_line = NULL;
 	}
 	return (NULL);
 }
 
-static char	*concatenate(char *new_str, char *heap_str, char *buf_str)
+static char	*concatenate(char *new_str, char *heap_str, char *buf_str, int len)
 {
 	int		index;
 
@@ -44,7 +43,7 @@ static char	*concatenate(char *new_str, char *heap_str, char *buf_str)
 		new_str[index] = heap_str[index];
 		index++;
 	}
-	while (buf_str && *buf_str)
+	while (buf_str && *buf_str && index <= len)
 	{
 		new_str[index] = *buf_str;
 		index++;
@@ -54,26 +53,33 @@ static char	*concatenate(char *new_str, char *heap_str, char *buf_str)
 	if (heap_str)
 	{
 		free(heap_str);
-		heap_str = NULL;
 	}
 	return (new_str);
 }
 
-static char	*form_nl(char *next_line, char *buffer, char *end_of_line)
+static char	*form_nl(char *next_line, char *buffer, char *newline)
 {
 	char	*new_string;
 	int		len;
 
 	len = 0;
-	if (end_of_line)
-		*(end_of_line + 1) = 0;
 	if (next_line)
 		len += ft_strlen(next_line);
 	len += ft_strlen(buffer);
+	if (newline)
+		len -= ft_strlen(newline + 1);
 	new_string = malloc(len + 1);
 	if (!new_string)
 		return (NULL);
-	return (concatenate(new_string, next_line, buffer));
+	if (newline)
+	{
+		new_string = concatenate(new_string, next_line, buffer, len);
+		buffer = newline + 1;
+		return (clean(new_string));
+	}
+	new_string = concatenate(new_string, next_line, buffer, len);
+	*buffer = 0;
+	return (new_string);
 }
 
 char	*get_next_line(int fd)
@@ -81,24 +87,24 @@ char	*get_next_line(int fd)
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*next_line;
 	int			bytes_read;
-	char		*end_of_line;
+	char		*newline;
 
 	if (check(fd, buffer))
 		return (NULL);
 	next_line = NULL;
 	while ("there is stuff to read and no newline characters found")
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-			return (clean(next_line));
-		buffer[bytes_read] = 0;
-		end_of_line = ft_strchr(buffer, '\n');
-		if (end_of_line)
-			return (clean(form_nl(next_line, buffer, end_of_line)));
-		end_of_line = ft_strchr(buffer, 0);
-		if (end_of_line)
-			return (clean(form_nl(next_line, buffer, end_of_line)));
-		next_line = form_nl(next_line, buffer, end_of_line);
+		if (!*buffer)
+		{
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
+			if (bytes_read <= 0)
+				return (clean(next_line));
+			buffer[bytes_read] = 0;
+		}
+		newline = ft_strchr(buffer, '\n');
+		if (newline)
+			return (form_nl(next_line, buffer, newline));
+		next_line = form_nl(next_line, buffer, newline);
 		if (!next_line)
 			return (clean(next_line));
 	}
