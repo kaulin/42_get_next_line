d4e4a2565c7d89175dbe995fac18d4d609ec44a0
Jaakko Junttila <jajuntti@student.hive.fi>
2023-12-01 10:57:05 +0200

    Fixes everything, adds files for bonus

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-static int	calculate_merged_length(char *line, char *buffer, char *end_of_line)
+static int	calculate_merged_length(char *line, char *buffer, char *line_end)
 {
 	int		len;
 
@@ -20,19 +20,19 @@ static int	calculate_merged_length(char *line, char *buffer, char *end_of_line)
 	if (line)
 		len += ft_strlen(line);
 	len += ft_strlen(buffer);
-	if (end_of_line)
-		len -= ft_strlen(end_of_line + 1);
+	if (line_end)
+		len -= ft_strlen(line_end + 1);
 	return (len);
 }
 
-static char	*merge(char *line, char *buffer, char *end_of_line)
+static char	*merge(char *line, char *buffer, char *line_end)
 {
 	int		old_len;
 	int		new_len;
 	char	*new_str;
 
 	old_len = 0;
-	new_len = calculate_merged_length(line, buffer, end_of_line);
+	new_len = calculate_merged_length(line, buffer, line_end);
 	new_str = malloc(new_len + 1);
 	if (!new_str)
 		return (clean(line, NULL));
@@ -44,18 +44,23 @@ static char	*merge(char *line, char *buffer, char *end_of_line)
 	if (buffer && *buffer)
 		ft_memcpy(new_str + old_len, buffer, new_len - old_len);
 	new_str[new_len] = 0;
+	if (!line_end)
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
 	return (clean(line, new_str));
 }
 
-static int	read_buffer(int fd, char *buffer, int *end_of_file)
+static int	read_buffer(int fd, char *buffer, int *file_end)
 {
 	int	bytes_read;
 
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
+	if (bytes_read < 0 || buffer == NULL)
+	{	
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
 		return (1);
+	}
 	if (bytes_read < BUFFER_SIZE)
-		*end_of_file = 1;
+		*file_end = 1;
 	buffer[bytes_read] = 0;
 	return (0);
 }
@@ -64,27 +69,26 @@ char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
-	int			end_of_file;
-	char		*end_of_line;
+	int			file_end;
+	char		*line_end;
 
 	line = NULL;
-	end_of_file = 0;
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	file_end = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
-		buffer[0] = 0;
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
 		return (NULL);
 	}
 	while ("There is stuff to do")
 	{
 		if (*buffer == 0)
-			if (read_buffer(fd, buffer, &end_of_file))
+			if (read_buffer(fd, buffer, &file_end) || (!line && *buffer == 0))
 				return (clean(line, NULL));
-		end_of_line = ft_strchr(buffer, '\n');
-		line = merge(line, buffer, end_of_line);
-		if (end_of_line)
-			ft_memcpy(buffer, end_of_line + 1, ft_strlen(end_of_line + 1) + 1);
-		if (!line || end_of_line || end_of_file)
+		line_end = ft_strchr(buffer, '\n');
+		line = merge(line, buffer, line_end);
+		if (line_end)
+			ft_memcpy(buffer, line_end + 1, ft_strlen(line_end + 1) + 1);
+		if (!line || line_end || file_end)
 			return (line);
-		buffer[0] = 0;
 	}
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-static int	calculate_merged_length(char *line, char *buffer, char *end_of_line)
+static int	calculate_merged_length(char *line, char *buffer, char *line_end)
 {
 	int		len;
 
@@ -20,22 +20,22 @@ static int	calculate_merged_length(char *line, char *buffer, char *end_of_line)
 	if (line)
 		len += ft_strlen(line);
 	len += ft_strlen(buffer);
-	if (end_of_line)
-		len -= ft_strlen(end_of_line + 1);
+	if (line_end)
+		len -= ft_strlen(line_end + 1);
 	return (len);
 }
 
-static char	*merge(char *line, char *buffer, char *end_of_line)
+static char	*merge(char *line, char *buffer, char *line_end)
 {
 	int		old_len;
 	int		new_len;
 	char	*new_str;
 
-	old_len;
-	new_len = calculate_merged_length(line, buffer, end_of_line);
+	old_len = 0;
+	new_len = calculate_merged_length(line, buffer, line_end);
 	new_str = malloc(new_len + 1);
 	if (!new_str)
-		return(clean(line, NULL));
+		return (clean(line, NULL));
 	if (line && *line)
 	{
 		old_len = ft_strlen(line);
@@ -44,46 +44,51 @@ static char	*merge(char *line, char *buffer, char *end_of_line)
 	if (buffer && *buffer)
 		ft_memcpy(new_str + old_len, buffer, new_len - old_len);
 	new_str[new_len] = 0;
+	if (!line_end)
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
 	return (clean(line, new_str));
 }
 
-static char	*read_recursively(int fd, char *buffer, char *line, int end_of_file)
+static int	read_buffer(int fd, char *buffer, int *file_end)
 {
-	int		bytes_read;
-	char	*merged_line;
-	char	*end_of_line;
+	int	bytes_read;
 
-	if (!*buffer)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-			return (clean(line, NULL));
-		buffer[bytes_read] = 0;
-		if (bytes_read < BUFFER_SIZE)
-			end_of_file = 1;
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0 || buffer == NULL)
+	{	
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
+		return (1);
 	}
-	end_of_line = ft_strchr(buffer, '\n');
-	merged_line = merge(line, buffer, end_of_line);
-	if (!merged_line)
-		return (NULL);
-	if (end_of_line)
-		ft_memcpy(buffer, end_of_line, ft_strlen(end_of_line + 1) + 1);
-	if (!end_of_line || !end_of_file)
-	{
-		buffer[0] = 0;
-		line = read_recursively(fd, buffer, merged_line, end_of_file);
-	}
-	return (line);
+	if (bytes_read < BUFFER_SIZE)
+		*file_end = 1;
+	buffer[bytes_read] = 0;
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	int			file_end;
+	char		*line_end;
 
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	line = NULL;
+	file_end = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
-		*buffer = 0;
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
 		return (NULL);
 	}
-	return (read_recursively(fd, buffer, NULL, 0));
+	while ("There is stuff to do")
+	{
+		if (*buffer == 0)
+			if (read_buffer(fd, buffer, &file_end) || (!line && *buffer == 0))
+				return (clean(line, NULL));
+		line_end = ft_strchr(buffer, '\n');
+		line = merge(line, buffer, line_end);
+		if (line_end)
+			ft_memcpy(buffer, line_end + 1, ft_strlen(line_end + 1) + 1);
+		if (!line || line_end || file_end)
+			return (line);
+	}
 }
